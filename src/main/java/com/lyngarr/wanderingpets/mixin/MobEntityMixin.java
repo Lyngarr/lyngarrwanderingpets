fdfb43d0e91952fd7155753de455d4a02d5fd7b8
Lyngarr <ulysse.gillotletang@gmail.com>
2025-03-15 15:14:43 +0100

    retrait d'imports inutiles

--- src/main/java/com/lyngarr/wanderingpets/mixin/MobEntityMixin.java
+++ src/main/java/com/lyngarr/wanderingpets/mixin/MobEntityMixin.java
@@ -1,22 +1,15 @@
 package com.lyngarr.wanderingpets.mixin;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.goal.FollowOwnerGoal;
 import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.passive.TameableEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MobEntity.class)
 public class MobEntityMixin extends EntityMixin {
